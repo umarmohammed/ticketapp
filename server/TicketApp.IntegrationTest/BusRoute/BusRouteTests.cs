f8b47af19cdcc866d1373e06caeb87ceb49d1321
Umar Mohammed <umar@teacherapp.co.uk>
2018-09-09 18:24:46 +0100

    ta32 add integration tests
    #32

---
+++ server/TicketApp.IntegrationTest/BusRoute/BusRouteTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TicketApp.Common.Extensions;
+using TicketApp.Domain.Command;
+using TicketApp.WebApi;
+using Xunit;
+
+namespace TicketApp.IntegrationTest.BusRoute
+{
+    public class BusRouteTests
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+
+        public BusRouteTests()
+        {
+            _server = new TestServer(
+                new WebHostBuilder()
+                    .UseStartup<TestStartup>());
+
+            _client = _server.CreateClient();
+        }
+
+
+        [Fact]
+        public async Task TestFind()
+        {
+            var requestUrl = "/api/busroute";
+
+            var result = await _client.GetAsync(requestUrl);
+            var resultString = await result.Content.ReadAsStringAsync();
+
+            var busRoutes = 
+                JsonConvert
+                    .DeserializeObject<IEnumerable<Domain.Model.BusRoute>>(resultString);
+
+            Assert.True(busRoutes.Count() == 2);
+            Assert.True(result.IsSuccessStatusCode);
+        }
+
+
+        [Fact]
+        public async Task TestCreate()
+        {
+            var requestUrl = "/api/busroute/create";
+            var cmd = new CreateBusRouteCommand
+            {
+                Name = "test3",
+                Price = 3.3
+            };
+
+
+            var result = await _client.PostAsync(requestUrl, cmd.ToJsonStringContent());
+            var resultString = await result.Content.ReadAsStringAsync();
+
+            var busRoute = JsonConvert.DeserializeObject<Domain.Model.BusRoute>(resultString);
+
+            Assert.True(result.IsSuccessStatusCode);
+        }
+
+    }
+}
---
+++ server/TicketApp.WebApi/TestStartup.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TicketApp.Schema.Model;
+
+namespace TicketApp.WebApi
+{
+    public class TestStartup 
+    {
+        public TestStartup(IConfiguration configuration) 
+        {
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<Entities>(options => 
+                options.UseInMemoryDatabase("TestDb"));
+
+            services.AddMvc();
+            services.AddAutoMapper();
+
+            Startup.RegisterServices(services);
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var context = app.ApplicationServices.GetService<Entities>();
+
+            AddTestData(context);
+            app.UseMvc();
+        }
+
+
+        private static void AddTestData(Entities context)
+        {
+            var testRoute1 = new BusRoute
+            {
+                Name = "Test1",
+                Price = 1.0
+            };
+
+            var testRoute2 = new BusRoute
+            {
+                Name = "Test2",
+                Price = 2.0
+            };
+
+            context.BusRoute.Add(testRoute1);
+            context.BusRoute.Add(testRoute2);
+
+            context.SaveChanges();
+        }
+    }
+}
